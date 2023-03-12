a9fc5c88fdb1112e9cd39e6ec180ab22bc0df909
tyrafero <lehanhunter@gmail.com>
2023-03-12 16:40:33 +0545

    Second Commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Feedback(models.Model):
+    name= models.CharField(max_length=100)
+    post= models.CharField(max_length=400)
+    image= models.ImageField(upload_to='media')
+    comment= models.TextField(blank=True)
